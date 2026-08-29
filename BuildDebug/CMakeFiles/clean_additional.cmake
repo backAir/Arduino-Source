bb288d68abb5e3e4127f7d027e68e75c3cf29f68
Anthony <72553201+backAir@users.noreply.github.com>
2026-08-29 11:12:03 +0200

    build thingies

---
+++ BuildDebug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,16 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\SerialProgramsCommandLine_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\SerialProgramsCommandLine_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\SerialProgramsLib_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\SerialProgramsLib_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\SerialPrograms_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\SerialPrograms_autogen.dir\\ParseCache.txt"
+  "SerialProgramsCommandLine_autogen"
+  "SerialProgramsLib_autogen"
+  "SerialPrograms_autogen"
+  )
+endif()
